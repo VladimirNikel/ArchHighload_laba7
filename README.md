74f4ad73690b3868798afe39aff5a2c2eedc5d11
VladimirNikel <vova_niemisto@mail.ru>
2020-11-24 03:01:13 +0300

    Работает в рамках локального серверва с одной бд

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 > 1. Создать ручку PUT/POST для сохранения погоды на сервер.
 > 2. Настроить сохранение всех данных о погоде в redis.
 > 3. Исправить ручку GET так, чтобы проверка данных проходила через redis. Если данных в redis нет, то их необходимо запросить у сервиса-источника погоды, сохранить в redis и отдать пользователю.
-> 4. Нужно настроить репликаию и шардирование для redis. Должно быть как минимум два шарда redis.
+> 4. Нужно настроить репликацию и шардирование для redis. Должно быть как минимум два шарда redis.
 > * можно использовать *Redis Cluster* или *Redis/Sentinel/temproxy* или *mcrouter/memcahed*
 > * проверить отказоустойчивость при падении мастера, наличие равномерного шардирования
 
@@ -23,7 +23,43 @@
 ## Инструкция по установке:
 1. Скачать/стянуть репозиторий
 1. Перейти в папку репозитория
+1. На сервере (а лучше в контейнере) выполнить установку redis при помощи следующих команд:
+```bash
+apt install make
 
+redisurl="http://download.redis.io/redis-stable.tar.gz"
+curl -s -o redis-stable.tar.gz $redisurl
+mkdir -p /usr/local/lib/
+chmod a+w /usr/local/lib/
+tar -C /usr/local/lib/ -xzf redis-stable.tar.gz
+rm redis-stable.tar.gz
+cd /usr/local/lib/redis-stable/
+make && make install								#какая-то ошибка тут
+
+ls -hFG /usr/local/bin/redis-* | sort
+/usr/local/bin/redis-benchmark*
+/usr/local/bin/redis-check-aof*
+/usr/local/bin/redis-check-rdb*
+/usr/local/bin/redis-cli*
+/usr/local/bin/redis-sentinel@
+/usr/local/bin/redis-server*
+
+mkdir -p /etc/redis/
+touch /etc/redis/6379.conf
+# /etc/redis/6379.conf
+ 
+echo "port              6379" > /etc/redis/6379.conf
+echo "daemonize         yes" >> /etc/redis/6379.conf
+echo "save              60 1" >> /etc/redis/6379.conf
+echo "bind              127.0.0.1" >> /etc/redis/6379.conf
+echo "tcp-keepalive     300" >> /etc/redis/6379.conf
+echo "dbfilename        dump.rdb" >> /etc/redis/6379.conf
+echo "dir               ./" >> /etc/redis/6379.conf
+echo "rdbcompression    yes" >> /etc/redis/6379.conf
+
+
+redis-server /etc/redis/6379.conf #Запуск самого сервера
+```
 
 
 
@@ -32,4 +68,5 @@
 - Установленные модули:
 	+ FastAPI `sudo pip3 install fastapi`
 	+ Unicorn `sudo pip3 install uvicorn`
+	+ Redis `sudo pip3 install redis`
 
---
+++ main.py
@@ -0,0 +1,97 @@
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+app = FastAPI()
+
+import uvicorn
+
+#необходимо, чтобы работать с json'ом
+import json
+
+#необходимо для работы с переменными окружения
+import os
+import sys
+
+#необходимо для работы с API openweathermap
+import pyowm
+from pyowm.utils.config import get_default_config
+from pyowm.utils import timestamps
+
+import redis
+
+#задание параметров redis'а
+redis_host, redis_port, redis_db = 'localhost', 6379, 0
+r = redis.Redis(host=redis_host, port=redis_port, db=redis_db)
+time_storage = 60*10				#10 минут - актуальность данных о погоде в городе
+
+config_dict = get_default_config()
+config_dict['language'] = 'ru'
+
+app_key = os.environ.get('OWM_APP_KEY')
+owm = pyowm.OWM(app_key, config_dict)
+
+my_id = sys.argv[1]
+
+def current_weather(city: str):
+	#обращение к внешнему сервису
+	mgr = owm.weather_manager()
+	observation = mgr.weather_at_place(city)
+	w = observation.weather
+	temp = w.temperature('celsius')['temp']
+
+	#занесение в бд данных
+	r.set(city, temp, ex=time_storage)
+
+	#возвращение текущей погоды
+	return temp
+
+def forecast_weather(city: str, timestamp: str):
+	#обращение к внешнему сервису
+	mgr = owm.weather_manager()
+	observation = mgr.forecast_at_place(city, "3h") #данной командой стягивается прогноз погоды на ближайшие 5 дней с частотой 3 часа
+	if timestamp == "1h":
+		time = timestamps.next_hour()
+	elif timestamp == "3h":
+		time = timestamps.next_three_hours() 
+	else:
+		time = timestamps.now();
+	w = observation.get_weather_at(time)
+	temp = w.temperature('celsius')['temp']
+
+	#занесение в бд данных (не совсем корректно, ведь у нас прогноз погоды...)
+	r.set(city, temp, ex=time_storage)
+
+	#возвращение текущей погоды
+	return temp
+
+def find_data(city: str, metod: bool, ts: str):
+	if type(r.get(city)) != type(None):
+		#если в базе найдено значение
+		print("city: ",city," взято из кэша и актуально: ", r.ttl(city))
+		return r.get(city).decode("utf-8")
+	else:
+		#если не найдено значение
+		print("city: ",city," ищем в API")
+		if metod == True:	#current weather
+			return current_weather(city)
+		else:				#forecast weather
+			return forecast_weather(city, ts)
+
+
+@app.get("/")
+def print_web():
+	html_content = open('./index.html', 'r').read()
+	return HTMLResponse(content=html_content, status_code=200)
+
+@app.get("/v1/current/")
+def current(city: str):
+	temp = find_data(city.lower(), True, "")
+	return json.dumps({"city": city,"unit": "celsius", "temperature": temp, "id_service": my_id})
+
+@app.get("/v1/forecast/")
+def forecast(city: str, timestamp: str):
+	temp = find_data(city.lower(), False, timestamp)
+	return json.dumps({"city": city,"unit": "celsius", "temperature": temp, "id_service": my_id})
+
+if __name__ == "__main__":
+	uvicorn.run(app, host="0.0.0.0", port=8000)
